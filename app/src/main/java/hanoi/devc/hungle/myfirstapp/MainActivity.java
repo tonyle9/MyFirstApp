65418797a64ef175ffe4c23858bf9316f6068212
tonyle9 <hung.lt@outlook.com>
2018-12-29 00:15:50 +0700

    first commit

---
+++ app/src/main/java/hanoi/devc/hungle/myfirstapp/MainActivity.java
@@ -0,0 +1,13 @@
+package hanoi.devc.hungle.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
